4ed43c95e4151262f83fadf64bf2129b5d9d27b4
Youssef Ahmed <youssefahmedd660@gmail.com>
2025-06-17 21:47:59 +0300

    feat: Add smart contract constants and addresses

---
+++ context/Constants.tsx
@@ -0,0 +1,13 @@
+import crowdfundingArtifact from "./Crowdfunding.json"
+import nftMinterArtifact from "./NFTMinter.json"
+
+// Contract Addresses
+export const CROWDFUNDING_ADDRESS = '0x8003D32D8ccC97B108C4Ed4Bc327315f52C49E76'
+export const NFT_MINTER_ADDRESS = '0x274625e8192e4cde0F54876a325e7B7CcA52e78C'
+export const VRF25_CONSUMER = '0x7cEb9095Ad00D6b155C0AaD30f02Fc8524C21Dc5'
+export const PRICE_DATAFEED = '0x18ad382B50f8BAE8010F3FBb3a98fCEd1279778C'
+export const LOGTRIAGER_AUTO = '0x536c0cFBF886e90ACbe342d9D8bbfaB037AA3c80'
+
+// ABIs
+export const CROWDFUNDING_ABI = crowdfundingArtifact.abi
+export const NFT_MINTER_ABI = nftMinterArtifact.abi
